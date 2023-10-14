80bfae6548b58ac7ec53988efb22e2436345fbeb
UltimateByte <git@lrob.fr>
2023-10-14 21:44:50 +0200

    Create testplugin.php

---
+++ testplugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package wp_testplugin
+ * @version 1.0.0
+ */
+/*
+Plugin Name: WP Test Plugin
+Plugin URI: http://wordpress.org/plugins/wp-testplugin
+Description: This is just a plugin.
+Author: Robin Labadie
+Version: 1.0.0
+Author URI: https://www.lrob.fr
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
